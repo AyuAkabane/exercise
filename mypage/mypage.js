b189939b08afa9acf858fa8bd6f62f68fc294938
Ayu AKABANE <87026230+AyuAkabane@users.noreply.github.com>
2022-05-13 15:22:15 +0900

    Update mypage.js

--- mypage/mypage.js
+++ mypage/mypage.js
@@ -3,3 +3,7 @@ function clickBtn1(){
 	location.href = "file:///Users/akabaneayu/Documents/%E3%82%AF%E3%82%A4%E3%82%B9%E3%82%99/index.html";
 
 }
+function clickBtn2(){
+
+	location.href = "https://m.nugu.jp/";
+}
